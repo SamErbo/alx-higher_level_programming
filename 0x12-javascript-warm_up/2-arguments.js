77a6500a6df44d672a2e2e9ed114d3cfd736c3b0
Samuel Erbo <33570394+samuelerbo@users.noreply.github.com>
2023-11-14 05:58:04 +0300

    fix typos

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -6,6 +6,6 @@ const len = argv.length;
 if (len === 2) {
   console.log('No argument');
 } else if(len === 3) {
-  console.log('Argument argument');
+  console.log('Argument found');
 } else{
-  console.log('Arguments Found');
+  console.log('Arguments found');
