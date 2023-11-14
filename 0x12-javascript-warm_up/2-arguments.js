dca123dae73b0589fdc12b56816ad7d2a1371bb9
samuelerbo <samuel.ermias@gmail.com>
2023-11-14 05:32:29 +0300

    remove extra equal sign

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,7 +3,7 @@ const { argv } = require('node:process');
 
 const len = argv.length;
 
-if (len==== 2) {
+if (len === 2) {
   console.log('No argument');
 } else if(len === 3) {
   console.log('Argument argument');
